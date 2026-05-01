7dce05dfb51a018379f32f9869bc6d39ebc492a3
devanshumrao9044 <224992621+devanshumrao9044@users.noreply.github.com>
2026-05-01 16:13:41 +0530

    Update appId and appName in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tera_naam.quizapp', // Isko tu apne hisaab se badal lena baad mein
-  appName: 'QuizApp',
+  appId: 'com.materialhub.app.', // Isko tu apne hisaab se badal lena baad mein
+  appName: 'Rankify',
   webDir: 'dist',
   plugins: {
     PushNotifications: {
